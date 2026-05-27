abb2d4478a1df53370d2188739e235f9ca1a976b
krxxg04 <u202312629@upc.edu.pe>
2026-05-26 22:08:15 -0500

    feat: enhance main application setup with environment loading, database connection, and service initialization.

--- main.go
+++ main.go
@@ -1,20 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"device-management-service/application/commandservices"
+	"device-management-service/application/eventhandlers"
+	"device-management-service/application/queryservices"
+	"device-management-service/domain/services"
+	appconfiguration "device-management-service/infrastructure/configuration"
+	kafkamessaging "device-management-service/infrastructure/messaging/kafka"
+	gormconfiguration "device-management-service/infrastructure/persistence/gorm/configuration"
+	gormrepositories "device-management-service/infrastructure/persistence/gorm/repositories"
+	"device-management-service/interfaces/rest"
+	"device-management-service/interfaces/rest/controllers"
+	"github.com/joho/godotenv"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found, using environment variables")
+	}
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	config := appconfiguration.LoadAppConfig()
+
+	db, err := gormconfiguration.ConnectDatabase(config.DatabaseURL)
+	if err != nil {
+		log.Fatalf("database connection failed: %v", err)
+	}
+	if err := gormconfiguration.AutoMigrate(db); err != nil {
+		log.Fatalf("database migration failed: %v", err)
+	}
+
+	deviceRepository := gormrepositories.NewDeviceGormRepository(db)
+	bindingRepository := gormrepositories.NewDeviceBindingGormRepository(db)
+	configurationRepository := gormrepositories.NewDeviceConfigurationGormRepository(db)
+	eventRepository := gormrepositories.NewDeviceEventGormRepository(db)
+
+	kafkaProducer := kafkamessaging.NewProducer(config.KafkaBrokers, config.KafkaClientID)
+	integrationEventHandler := eventhandlers.NewDeviceIntegrationEventHandler(kafkaProducer)
+	deviceDomainService := services.NewDeviceDomainService()
+
+	deviceCommandService := commandservices.NewDeviceCommandService(deviceRepository, integrationEventHandler)
+	bindingCommandService := commandservices.NewDeviceBindingCommandService(deviceRepository, bindingRepository, deviceDomainService, integrationEventHandler)
+	configurationCommandService := commandservices.NewDeviceConfigurationCommandService(deviceRepository, configurationRepository, deviceDomainService, integrationEventHandler)
+	eventCommandService := commandservices.NewDeviceEventCommandService(deviceRepository, eventRepository, integrationEventHandler)
+
+	deviceQueryService := queryservices.NewDeviceQueryService(deviceRepository)
+	bindingQueryService := queryservices.NewDeviceBindingQueryService(bindingRepository)
+	configurationQueryService := queryservices.NewDeviceConfigurationQueryService(configurationRepository)
+	eventQueryService := queryservices.NewDeviceEventQueryService(eventRepository)
+
+	router := rest.NewRouter(rest.RouterDependencies{
+		DeviceController:        controllers.NewDeviceController(deviceCommandService, deviceQueryService),
+		BindingController:       controllers.NewBindingController(bindingCommandService, bindingQueryService),
+		ConfigurationController: controllers.NewConfigurationController(configurationCommandService, configurationQueryService),
+		EventController:         controllers.NewEventController(eventCommandService, eventQueryService),
+		CORSAllowedOrigins:      config.CORSAllowedOrigins,
+	})
+
+	log.Printf("device-management-service running on port %s", config.Port)
+	if err := router.Run(":" + config.Port); err != nil {
+		log.Fatalf("server failed: %v", err)
 	}
 }
